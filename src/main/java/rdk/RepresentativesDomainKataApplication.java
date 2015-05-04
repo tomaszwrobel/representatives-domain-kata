cb76a8babf427e3cf4e2d1b3024d7c486f6b49cd
Tomasz Wróbel <tomasz.wrobel@cohesiva.com>
2015-05-04 13:41:41 +0200

    initial projct setup

---
+++ src/main/java/rdk/RepresentativesDomainKataApplication.java
@@ -0,0 +1,12 @@
+package rdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepresentativesDomainKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepresentativesDomainKataApplication.class, args);
+    }
+}
